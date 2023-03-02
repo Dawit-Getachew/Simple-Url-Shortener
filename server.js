31a75ca81a914ccd890062cfc8c2648a3f5a3f50
Dawit-Getachew <Dawitgetachewat123@gmail.com>
2023-03-02 18:40:55 +0300

    Simple Url Shortener

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const shortUrl = require('./models/shortUrls')
+
+const app = express()
+
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology:true
+})
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async (req, res) => {
+    const shortUrls = await shortUrl.find()
+    res.render('index', {shortUrls: shortUrls});
+})
+
+app.post('/shortUrls', async (req, res)=> {
+    const resp = {
+        author: req.body.author,
+        full: req.body.fullUrl
+    }
+    await shortUrl.create(resp)
+    res.redirect('/')
+})
+
+app.get('/:shortUrlss', async (req, res) => {
+    const shortUrls = await shortUrl.findOne({short: req.params.shortUrlss})
+    if(shortUrls == null) return res.redirect('/')
+
+
+    shortUrls.clicks++
+    shortUrls.save()
+
+    res.redirect(shortUrls.full)
+})
+
+app.listen(process.env.PORT || 5000);
